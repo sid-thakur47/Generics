c432d845a134fa1dfe3ae0ec2304a59de917c0b6
sid-thakur47 <sidthakur258@gmail.com>
2020-04-27 17:09:47 +0530

    ADDED:Functionality to print arrays using generic

--- Demo/src/com/bl/PrintArray.java
+++ Demo/src/com/bl/PrintArray.java
@@ -1,30 +1,18 @@
 package com.bl;
 
-public class PrintArray {
-
-    public static void printArray(char[] chars) {
-        for (char c : chars) {
-            System.out.println( c );
-        }
-    }
-    public static void printArray(int[] intArray) {
-        for (int c : intArray) {
-            System.out.println( c );
-        }
-    }
-
-    public static void printArray(double[] doubles) {
-        for (double c : doubles) {
-            System.out.println( c );
-        }
-    }
+public class PrintArray<E> {
 
     public static void main(String[] args) {
-        int[] intArray = {1, 2, 3};
-        double[] doubles = {1.1, 2.1, 3.0};
-        char[] chars = {'a', 'b'};
-        printArray( chars );
-        printArray( intArray );
-        printArray( doubles );
+        Integer[] intArray = {1, 2, 3};
+        Double[] doubleArray = {1.1, 2.1, 3.0};
+        Character[] charArray = {'a', 'b', 'c'};
+        new PrintArray<Integer>().printArray( intArray );
+        new PrintArray<Double>().printArray( doubleArray );
+        new PrintArray<Character>().printArray( charArray );
+    }
+    public void printArray(E[] chars) {
+        for (E c : chars) {
+            System.out.println( c );
+        }
     }
 }
